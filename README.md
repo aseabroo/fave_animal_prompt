2c9c4df972872377e5b5039908891c318a7b135d
Augustus <august.seabrooke@gmail.com>
2020-09-16 01:59:56 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a simple code that prompts the user what their name is 
+and what their favorite animal is. 
---
+++ name_animal_prompt.py
@@ -0,0 +1,9 @@
+# Author: August Seabrooke
+# Date: 9/15/2020
+# Description: Asks the user for their name, says a greeting, and
+#  asks what the user's favorite animal is, and then prints that out.
+
+name = input('Please enter your name: ')
+print('Hi '+name+'.')
+animal = input('What is your favorite animal? ')
+print('Your favorite animal is the '+animal+'.')
